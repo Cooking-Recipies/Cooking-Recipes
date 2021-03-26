67b35b972d128831799baae8e9ab65a8a9a28c69
justyn <justyn.chr1@gmail.com>
2021-03-26 17:33:46 +0100

    #15 added routes for photos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Routing\Router;
 
 $router = app(Router::class);
@@ -8,4 +9,9 @@ $router = app(Router::class);
 $router->post("/login", [AuthenticationController::class, "login"]);
 $router->post("/register", [AuthenticationController::class, "register"]);
 
+$router->middleware("auth:sanctum")->group(function (Router $router): void {
+    $router->post("/photos", [PhotoController::class, "create"]);
+    $router->get("/photos/user/{user}", [PhotoController::class, "index"]);
+    $router->delete("/photos/{photo}", [PhotoController::class, "delete"])->middleware("can:haveAccess,photo");
 
+});
