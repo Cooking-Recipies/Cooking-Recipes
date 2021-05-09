77c04505ab107b61b2f400fdf5041d4f86218bbc
Justyn Chroł <justyn.chr1@gmail.com>
2021-05-09 14:13:06 +0200

    #28 implemented functionality

---
+++ app/Http/Requests/UpdateRecipeRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\PhotoExists;
+
+class UpdateRecipeRequest extends ApiRequest
+{
+    public function rules(): array
+    {
+        return [
+            "title" => [
+                "string",
+            ],
+            "category_id" => [
+                "exists:categories,id",
+            ],
+            "number_of_people" => [
+            ],
+            "preparing_time" => [
+                "string",
+            ],
+            "instruction" => [
+                "string",
+            ],
+            "tags.*" => [
+                "string",
+            ],
+            "components" => [
+                "array",
+            ],
+            "components.*.name" => [
+                "required",
+                "string",
+            ],
+            "components.*.quantity" => [
+                "required",
+                "string",
+            ],
+            "photos.*" => [
+                "string",
+                new PhotoExists($this->user()),
+            ],
+        ];
+    }
+}
